bb1eeae67645cdd6e8599391197224aa7a1679b9
Tim Quattrochi <timq82@gmail.com>
2024-04-06 20:05:09 -0400

    pg will use process.env for values

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,16 +1,5 @@
 import { Pool } from "pg";
-import { ConnectionConfig } from "pg";
-import {
-  DB_PASSWORD,
-  DB_HOST,
-  DB_NAME,
-  DB_USER,
-} from "../utils/constants";
 
-export const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: 5432,
-});
+// the values for host, user, database, password, port are taken from the .env file
+export const pool = new Pool();
+
