ac70ac69362c22c5d09c8a6fca981a32536e839d
Tim Quattrochi <timq82@gmail.com>
2024-04-06 20:26:40 -0400

    add types

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const JWT_SECRET = String(process.env.JWT_SECRET);
+export const DB_USER = String(process.env.DB_USER);
+export const DB_NAME = String(process.env.DB_NAME);
+export const DB_PASSWORD = String(process.env.DB_PASSWORD);
+export const DB_HOST = String(process.env.DB_HOST);
+export const DB_PORT = Number(process.env.DB_PORT);
+export const PORT = Number(process.env.PORT);
